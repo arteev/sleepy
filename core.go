ef11d61e7eaa50f26371e0d4722a5f9e118e502e
Arteev Aleksey <alarteev@yandex.ru>
2017-04-18 15:52:04 +0500

    fix StartAddr

--- core.go
+++ core.go
@@ -1,7 +1,9 @@
 package sleepy
 
 import (
+	"context"
 	"encoding/json"
+	"encoding/xml"
 	"errors"
 	"fmt"
 	"net/http"
@@ -20,37 +22,37 @@ const (
 // GetSupported is the interface that provides the Get
 // method a resource must support to receive HTTP GETs.
 type GetSupported interface {
-	Get(url.Values, http.Header) (int, interface{}, http.Header)
+	Get(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // PostSupported is the interface that provides the Post
 // method a resource must support to receive HTTP POSTs.
 type PostSupported interface {
-	Post(url.Values, http.Header) (int, interface{}, http.Header)
+	Post(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // PutSupported is the interface that provides the Put
 // method a resource must support to receive HTTP PUTs.
 type PutSupported interface {
-	Put(url.Values, http.Header) (int, interface{}, http.Header)
+	Put(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // DeleteSupported is the interface that provides the Delete
 // method a resource must support to receive HTTP DELETEs.
 type DeleteSupported interface {
-	Delete(url.Values, http.Header) (int, interface{}, http.Header)
+	Delete(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // HeadSupported is the interface that provides the Head
 // method a resource must support to receive HTTP HEADs.
 type HeadSupported interface {
-	Head(url.Values, http.Header) (int, interface{}, http.Header)
+	Head(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // PatchSupported is the interface that provides the Patch
 // method a resource must support to receive HTTP PATCHs.
 type PatchSupported interface {
-	Patch(url.Values, http.Header) (int, interface{}, http.Header)
+	Patch(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // An API manages a group of resources by routing requests
@@ -60,15 +62,25 @@ type PatchSupported interface {
 // You can instantiate multiple APIs on separate ports. Each API
 // will manage its own set of resources.
 type API struct {
-	mux     *http.ServeMux
-	muxInitialized bool
+	mux                *http.ServeMux
+	muxInitialized     bool
+	marshal            Marshaler
+	marshalInitialized bool
 }
+
+type Marshaler func(v interface{}, prefix, indent string) ([]byte, error)
 
 // NewAPI allocates and returns a new API.
 func NewAPI() *API {
-	return &API{}
+	return &API{
+		marshal: json.MarshalIndent,
+	}
 }
 
+func (api *API) SetMarshaler(m Marshaler) {
+	api.marshalInitialized = true
+	api.marshal = m
+}
 func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 	return func(rw http.ResponseWriter, request *http.Request) {
 
@@ -77,7 +89,7 @@ func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 			return
 		}
 
-		var handler func(url.Values, http.Header) (int, interface{}, http.Header)
+		var handler func(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 
 		switch request.Method {
 		case GET:
@@ -111,9 +123,33 @@ func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 			return
 		}
 
-		code, data, header := handler(request.Form, request.Header)
+		code, data, header := handler(request.Context(), request.Form, request.Header)
 
-		content, err := json.MarshalIndent(data, "", "  ")
+		var (
+			content []byte
+			err     error
+		)
+
+		if api.marshalInitialized {
+			content, err = api.marshal(data, "", "  ")
+		} else {
+			contentType := "application/json"
+			if cth, exists := header["Content-type"]; exists {
+				contentType = cth[0]
+			}
+			switch contentType {
+			case "application/json":
+				content, err = json.MarshalIndent(data, "", "  ")
+			case "application/xml":
+				content, err = xml.MarshalIndent(data, "", "  ")
+				if err != nil {
+					content = []byte(xml.Header + string(content))
+				}
+			default:
+				panic(errors.New("unknown Content-type"))
+			}
+		}
+
 		if err != nil {
 			rw.WriteHeader(http.StatusInternalServerError)
 			return
@@ -133,11 +169,10 @@ func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 func (api *API) Mux() *http.ServeMux {
 	if api.muxInitialized {
 		return api.mux
-	} else {
-		api.mux = http.NewServeMux()
-		api.muxInitialized = true
-		return api.mux
 	}
+	api.mux = http.NewServeMux()
+	api.muxInitialized = true
+	return api.mux
 }
 
 // AddResource adds a new resource to an API. The API will route
@@ -165,4 +200,12 @@ func (api *API) Start(port int) error {
 	}
 	portString := fmt.Sprintf(":%d", port)
 	return http.ListenAndServe(portString, api.Mux())
+}
+
+func (api *API) StartAddr(addr string) error {
+	if !api.muxInitialized {
+		return errors.New("You must add at least one resource to this API.")
+	}
+
+	return http.ListenAndServe(addr, api.Mux())
 }
