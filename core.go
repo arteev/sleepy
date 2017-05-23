faeab138c480acb138b83c7fae1061d1b7ddf2d0
Arteev Aleksey <alarteev@yandex.ru>
2017-05-23 09:21:38 +0500

    Option. Added some tests

--- core.go
+++ core.go
@@ -3,13 +3,15 @@ package sleepy
 import (
 	"context"
 	"encoding/json"
-	"encoding/xml"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+	"sync"
 )
 
+//The definition of methods http
 const (
 	GET    = "GET"
 	POST   = "POST"
@@ -19,10 +21,21 @@ const (
 	PATCH  = "PATCH"
 )
 
+//Errors
+var (
+	ErrNotDefinedResource = errors.New("You must add at least one resource to this API")
+)
+
 // GetSupported is the interface that provides the Get
 // method a resource must support to receive HTTP GETs.
 type GetSupported interface {
 	Get(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
+}
+
+//GetSupportedWoCtx is the interface that provides the Get
+// method a resource must support to receive HTTP GETs
+type GetSupportedWoCtx interface {
+	Get(url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // PostSupported is the interface that provides the Post
@@ -31,10 +44,22 @@ type PostSupported interface {
 	Post(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
+// PostSupportedWoCtx is the interface that provides the Post
+// method a resource must support to receive HTTP POSTs.
+type PostSupportedWoCtx interface {
+	Post(url.Values, http.Header) (int, interface{}, http.Header)
+}
+
 // PutSupported is the interface that provides the Put
 // method a resource must support to receive HTTP PUTs.
 type PutSupported interface {
 	Put(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
+}
+
+// PutSupportedWoCtx is the interface that provides the Put
+// method a resource must support to receive HTTP PUTs.
+type PutSupportedWoCtx interface {
+	Put(url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // DeleteSupported is the interface that provides the Delete
@@ -43,16 +68,34 @@ type DeleteSupported interface {
 	Delete(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
+// DeleteSupportedWoCtx is the interface that provides the Delete
+// method a resource must support to receive HTTP DELETEs.
+type DeleteSupportedWoCtx interface {
+	Delete(url.Values, http.Header) (int, interface{}, http.Header)
+}
+
 // HeadSupported is the interface that provides the Head
 // method a resource must support to receive HTTP HEADs.
 type HeadSupported interface {
 	Head(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
 }
 
+// HeadSupportedWoCtx is the interface that provides the Head
+// method a resource must support to receive HTTP HEADs.
+type HeadSupportedWoCtx interface {
+	Head(url.Values, http.Header) (int, interface{}, http.Header)
+}
+
 // PatchSupported is the interface that provides the Patch
 // method a resource must support to receive HTTP PATCHs.
 type PatchSupported interface {
 	Patch(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
+}
+
+// PatchSupportedWoCtx is the interface that provides the Patch
+// method a resource must support to receive HTTP PATCHs.
+type PatchSupportedWoCtx interface {
+	Patch(url.Values, http.Header) (int, interface{}, http.Header)
 }
 
 // An API manages a group of resources by routing requests
@@ -62,94 +105,91 @@ type PatchSupported interface {
 // You can instantiate multiple APIs on separate ports. Each API
 // will manage its own set of resources.
 type API struct {
-	mux                *http.ServeMux
-	muxInitialized     bool
-	marshal            Marshaler
-	marshalInitialized bool
+	mux            *http.ServeMux
+	muxInitialized bool
+	once           sync.Once
+	marshal        map[string]Marshaler
 }
 
+//A Marshaler it is type for set up the option of  serialization  API
 type Marshaler func(v interface{}, prefix, indent string) ([]byte, error)
 
-// NewAPI allocates and returns a new API.
-func NewAPI() *API {
-	return &API{
-		marshal: json.MarshalIndent,
+//A Option using for options for API
+type Option func(a *API)
+
+//WithMarshaler using for setup a Marshaler for API. Name = Content-Type
+func WithMarshaler(name string, m Marshaler) Option {
+	return func(a *API) {
+		a.marshal[name] = m
 	}
 }
 
-func (api *API) SetMarshaler(m Marshaler) {
-	api.marshalInitialized = true
-	api.marshal = m
+// NewAPI allocates and returns a new API.
+func NewAPI() *API {
+	api := &API{
+		marshal: map[string]Marshaler{"application/json": json.MarshalIndent},
+	}
+
+	return api
 }
+
+type (
+	handlerType      func(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
+	handlerTypeWoCtx func(url.Values, http.Header) (int, interface{}, http.Header)
+)
+
+func (api *API) detectHandler(request *http.Request, resource interface{}) (handler handlerType) {
+	decorateContext := func(h handlerTypeWoCtx) handlerType {
+		return func(ctx context.Context, u url.Values, hdr http.Header) (int, interface{}, http.Header) {
+			return h(u, hdr)
+		}
+	}
+	switch request.Method {
+	case GET:
+		if resconcrete, ok := resource.(GetSupported); ok {
+			handler = resconcrete.Get
+		} else if resconcrete, ok := resource.(GetSupportedWoCtx); ok {
+			handler = decorateContext(resconcrete.Get)
+		}
+	case POST:
+		if resconcrete, ok := resource.(PostSupported); ok {
+			handler = resconcrete.Post
+		} else if resconcrete, ok := resource.(PostSupportedWoCtx); ok {
+			handler = decorateContext(resconcrete.Post)
+		}
+	case PUT:
+		if resconcrete, ok := resource.(PutSupported); ok {
+			handler = resconcrete.Put
+		}
+	case DELETE:
+		if resconcrete, ok := resource.(DeleteSupported); ok {
+			handler = resconcrete.Delete
+		}
+	case HEAD:
+		if resconcrete, ok := resource.(HeadSupported); ok {
+			handler = resconcrete.Head
+		}
+	case PATCH:
+		if resconcrete, ok := resource.(PatchSupported); ok {
+			handler = resconcrete.Patch
+		}
+	}
+	return
+}
+
 func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 	return func(rw http.ResponseWriter, request *http.Request) {
-
 		if request.ParseForm() != nil {
 			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
-
-		var handler func(context.Context, url.Values, http.Header) (int, interface{}, http.Header)
-
-		switch request.Method {
-		case GET:
-			if resource, ok := resource.(GetSupported); ok {
-				handler = resource.Get
-			}
-		case POST:
-			if resource, ok := resource.(PostSupported); ok {
-				handler = resource.Post
-			}
-		case PUT:
-			if resource, ok := resource.(PutSupported); ok {
-				handler = resource.Put
-			}
-		case DELETE:
-			if resource, ok := resource.(DeleteSupported); ok {
-				handler = resource.Delete
-			}
-		case HEAD:
-			if resource, ok := resource.(HeadSupported); ok {
-				handler = resource.Head
-			}
-		case PATCH:
-			if resource, ok := resource.(PatchSupported); ok {
-				handler = resource.Patch
-			}
-		}
-
+		var handler = api.detectHandler(request, resource)
 		if handler == nil {
 			rw.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-
 		code, data, header := handler(request.Context(), request.Form, request.Header)
-
-		var (
-			content []byte
-			err     error
-		)
-
-		if api.marshalInitialized {
-			content, err = api.marshal(data, "", "  ")
-		} else {
-			contentType := "application/json"
-			if cth, exists := header["Content-type"]; exists {
-				contentType = cth[0]
-			}
-			switch contentType {
-			case "application/json":
-				content, err = json.MarshalIndent(data, "", "  ")
-			case "application/xml":
-				content, err = xml.MarshalIndent(data, "", "  ")
-				if err != nil {
-					content = []byte(xml.Header + string(content))
-				}
-			default:
-				panic(errors.New("unknown Content-type"))
-			}
-		}
-
+		content, err := api.doMarshal(data, header)
 		if err != nil {
 			rw.WriteHeader(http.StatusInternalServerError)
 			return
@@ -164,14 +204,31 @@ func (api *API) requestHandler(resource interface{}) http.HandlerFunc {
 	}
 }
 
+func (api *API) doMarshal(data interface{}, header http.Header) (content []byte, err error) {
+	contentType := "application/json"
+	marshal := json.MarshalIndent
+	if h, exists := header["Content-type"]; exists {
+		contentType = h[0]
+		if idx := strings.Index(contentType, ";"); idx != -1 {
+			contentType = contentType[:idx]
+		}
+	}
+	if api.marshal != nil {
+		if m, exists := api.marshal[contentType]; exists {
+			marshal = m
+		}
+	}
+	content, err = marshal(data, "", "  ")
+	return
+}
+
 // Mux returns the http.ServeMux used by an API. If a ServeMux has
 // does not yet exist, a new one will be created and returned.
 func (api *API) Mux() *http.ServeMux {
-	if api.muxInitialized {
-		return api.mux
-	}
-	api.mux = http.NewServeMux()
-	api.muxInitialized = true
+	api.once.Do(func() {
+		api.mux = http.NewServeMux()
+		api.muxInitialized = true
+	})
 	return api.mux
 }
 
@@ -193,19 +250,18 @@ func (api *API) AddResourceWithWrapper(resource interface{}, wrapper func(handle
 	}
 }
 
-// Start causes the API to begin serving requests on the given port.
-func (api *API) Start(port int) error {
-	if !api.muxInitialized {
-		return errors.New("You must add at least one resource to this API.")
-	}
-	portString := fmt.Sprintf(":%d", port)
-	return http.ListenAndServe(portString, api.Mux())
+// Start causes the API to begin serving requests on the given port with options.
+func (api *API) Start(port int, opts ...Option) error {
+	return api.StartAddr(fmt.Sprintf(":%d", port), opts...)
 }
 
-func (api *API) StartAddr(addr string) error {
+// StartAddr causes the API to begin serving requests on the address with options.
+func (api *API) StartAddr(addr string, opts ...Option) error {
 	if !api.muxInitialized {
-		return errors.New("You must add at least one resource to this API.")
+		return ErrNotDefinedResource
 	}
-
+	for _, opt := range opts {
+		opt(api)
+	}
 	return http.ListenAndServe(addr, api.Mux())
 }
--- core_test.go
+++ core_test.go
@@ -1,10 +1,13 @@
 package sleepy
 
 import (
+	"encoding/xml"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
 	"testing"
+	"time"
 )
 
 type Item struct{}
@@ -16,18 +19,103 @@ func (item Item) Get(values url.Values, headers http.Header) (int, interface{}, 
 }
 
 func TestBasicGet(t *testing.T) {
-
 	item := new(Item)
-
 	var api = NewAPI()
 	api.AddResource(item, "/items", "/bar", "/baz")
-	go api.Start(3000)
+	go func() {
+		api.Start(3000)
+	}()
+	<-time.After(500 * time.Millisecond)
 	resp, err := http.Get("http://localhost:3000/items")
 	if err != nil {
 		t.Error(err)
 	}
+	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	if string(body) != "{\n  \"items\": [\n    \"item1\",\n    \"item2\"\n  ]\n}" {
 		t.Error("Not equal.")
+		t.Log(string(body))
+	}
+
+}
+
+type ItemCT struct {
+	contentType http.Header
+}
+type DataExport struct {
+	Name string
+}
+
+func (item ItemCT) Get(values url.Values, headers http.Header) (int, interface{}, http.Header) {
+	items := []DataExport{
+		{"Name1"},
+		{"Name2"},
+	}
+	return 200, items, item.contentType
+}
+
+func TestContentType(t *testing.T) {
+	item := new(ItemCT)
+	var api = NewAPI()
+	api.AddResource(item, "/items", "/bar", "/baz")
+	go func() {
+		api.Start(3002,
+			WithMarshaler("application/xml", xml.MarshalIndent),
+		)
+	}()
+	<-time.After(500 * time.Millisecond)
+	cases := []struct {
+		Contenttype http.Header
+		Result      string
+	}{
+		{nil, "[\n  {\n    \"Name\": \"Name1\"\n  },\n  {\n    \"Name\": \"Name2\"\n  }\n]"},
+		{map[string][]string{"Content-type": {"application/json"}}, "[\n  {\n    \"Name\": \"Name1\"\n  },\n  {\n    \"Name\": \"Name2\"\n  }\n]"},
+		{map[string][]string{"Content-type": {"application/xml"}}, "<DataExport>\n  <Name>Name1</Name>\n</DataExport>\n<DataExport>\n  <Name>Name2</Name>\n</DataExport>"},
+		{map[string][]string{"Content-type": {"application/xml;charset=utf-8"}}, "<DataExport>\n  <Name>Name1</Name>\n</DataExport>\n<DataExport>\n  <Name>Name2</Name>\n</DataExport>"},
+	}
+
+	for _, c := range cases {
+		item.contentType = c.Contenttype
+		resp, err := http.Get("http://localhost:3002/items")
+		if err != nil {
+			t.Error(err)
+		}
+		body, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if string(body) != c.Result {
+			ct, ok := c.Contenttype["Content-type"]
+			if !ok {
+				ct = []string{"<nil>"}
+			}
+			t.Errorf("Not equal for Content-type:%s", ct[0])
+		}
+	}
+
+}
+
+var once sync.Once
+
+func prepareBenchmark(b *testing.B) {
+	once.Do(func() {
+		item := new(Item)
+		var api = NewAPI()
+		api.AddResource(item, "/items", "/bar", "/baz")
+		go func() {
+			api.Start(3003)
+		}()
+	})
+}
+
+func BenchmarkBasicGet(b *testing.B) {
+	prepareBenchmark(b)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		resp, err := http.Get("http://localhost:3003/items")
+		if err != nil {
+			b.Error(err)
+		}
+		if err := resp.Body.Close(); err != nil {
+			b.Fatal(err)
+		}
 	}
 }
